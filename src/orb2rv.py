829c737cf1cbaa66927a054b328df629c7bfbf49
Mario Alvarez Redondo <marioar@gmail.com>
2023-01-07 19:50:04 +0100

    obr2rv

--- src/orb2rv.py
+++ src/orb2rv.py
@@ -6,37 +6,84 @@ from src.Orbit import Orbit
 class Orb2rv(Orbit):
     '''Orbital parameters to position and velocity vectors'''
 
-    def __init__(self, h, e, Omega, omega, i, nu, body):
+    def __init__(self,a=None, h=None, e=None, Omega=None, omega=None, i=None, nu=None, M=None, p=None, n=None, body=None):
         '''Initialize the class
         Parameters
         ----------
+        a : float
+            Semi-major axis in km
         h : float
-            Specific angular momentum
+            Specific angular momentum in km^2/s
         e : float
             Eccentricity
         Omega : float
-            Right ascension of the ascending node
+            Right ascension of the ascending node in degrees
         omega : float
-            Argument of periapsis
+            Argument of periapsis in degrees
         i : float
-            Inclination
+            Inclination in degrees
         nu : float
-            True anomaly
+            True anomaly in degrees
+        M : float
+            Mean anomaly
+        p : float
+            Semi-latus rect
+        n : float
+            Mean motion in rad/s
         body : Body
             Body object
-            '''
-            
+        '''
+
+        # Verify that at least 6 parameters are defined
+        ver =(a is None) + (h is None) + (e is None) + (Omega is None)
+        ver += (omega is None) + (i is None) + (nu is None) + (M is None) + (p is None) + (n is None)
+        if  ver < 5:
+            Warning('At least 6 parameters must be defined')
+
+        self.a = a
         self.h = h
+        self.n = n
         self.e = e
-        self.Omega = Omega
-        self.omega = omega
-        self.i = i
-        self.nu = nu
+
+        if a is not None:
+            self.a = a
+        elif self.h is not None:
+            self.a = self.h**2 / body.mu
+        elif self.n is not None:
+            self.a = (body.mu / self.n**2)**(1/3)
+        else:
+            raise ValueError('Either a, h or e must be defined')
+
+        self.p = p
+
+        if h is not None:
+            self.h = h
+        elif self.p is not None:
+            self.h = self.hfromp(self.p)
+        elif (self.e is not None) and (self.a is not None):
+            self.h = (self.a * body.mu * (1 - self.e**2))**0.5
+        else:
+            raise ValueError('Either h, p or a and e must be defined')
+        
+        self.Omega = Omega * np.pi / 180
+        self.omega = omega * np.pi / 180
+        self.i = i * np.pi / 180
+        self.M = M
+
+        if nu is not None:
+            self.nu = nu * np.pi / 180
+        elif self.M is not None:
+            self.M = self.M * np.pi / 180
+            self.nu = self.nufromM()
+        else:
+            raise ValueError('Either nu or M must be defined')
+
         self.mu = body.mu
         self.radius = body.radius
 
         self.r = self.position_eci()
         self.v = self.velocity_eci()
+
 
     def position(self):
         '''Position vector 4.37'''
@@ -73,6 +120,26 @@ class Orb2rv(Orbit):
     def hfromp(self):
         '''Get h from p'''
         return np.sqrt(self.mu * self.p)
+
+    def nufromM(self):
+        '''Get nu from M'''
+        e = self.e
+        M = self.M
+
+        E = self.EfromM()
+        return 2 * np.arctan(np.sqrt((1 + e) / (1 - e)) * np.tan(E / 2))
+
+    def EfromM(self):
+        '''Get E from M'''
+        e = self.e
+        M = self.M
+
+        E = M + e * np.sin(M)
+        E_old = E + 1
+        while np.abs(E - E_old) > 1e-8:
+            E_old = E
+            E = E - (E - e * np.sin(E) - M) / (1 - e * np.cos(E))
+        return E
 
 
     
