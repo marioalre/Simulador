6a3a2c39252f295ac98071e47bd6b68a01127e4e
Mario Alvarez Redondo <marioar@gmail.com>
2023-04-26 17:27:12 +0200

    GUI creada

--- scripts/GUI.py
+++ scripts/GUI.py
@@ -4,78 +4,181 @@
 # Path: scripts\GUI.py
 
 import tkinter as tk
-from tkinter import ttk
 import webbrowser
+from tkinter import *
 
-def oppen_web():
+import utils as U
+
+def linkedin():
     webbrowser.open_new("linkedin.com/in/mario-álvarez-redondo")
 
+def github():
+    webbrowser.open_new("https://github.com/marioalre")
 
-class GUI(tk.Tk):
+
+class App(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.title("Orbit")
-        self.geometry("1080x720")
+
+        ## Setting up Initial Things
+        self.title("GUI UniLeón")
+        self.geometry("720x550")
         self.resizable(True, True)
-        self.config(bg="white")
-        self.iconbitmap("scripts\images\escudoEIIIA.ico")
+        self.iconphoto(True, tk.PhotoImage(file="scripts/images/escudoEIIIA.png"))
+    
+        ## Creating a container
+        container = tk.Frame(self) # bg white
+        container.pack(side="top", fill="both", expand = True)
+        container.grid_rowconfigure(10, weight=1)
+        container.grid_columnconfigure(10, weight=1)
 
-        self.create_widgets()
 
-    def create_widgets(self):
-        self.create_menu()
-        self.create_notebook()
+        ## Initialize Frames
+        self.frames = {}
+        self.HomePage = HomePage
+        self.PreDetOrb = PreDetOrb
+        self.Mission = Mission
 
-    def create_menu(self):
-        self.menu = tk.Menu(self)
-        self.config(menu=self.menu)
+        ## Defining Frames and Packing it
+        for F in {HomePage, PreDetOrb, Mission}:
+            frame = F(self, container)
+            self.frames[F] = frame
+            frame.grid(row=10, column=10, sticky="nsew")    
+           
+        self.show_frame(HomePage)
 
-        self.file_menu = tk.Menu(self.menu, tearoff=0)
-        self.menu.add_cascade(label="File", menu=self.file_menu)
-        self.file_menu.add_command(label="New")
-        self.file_menu.add_command(label="Open")
-        self.file_menu.add_command(label="Save")
-        self.file_menu.add_command(label="Save as...")
-        self.file_menu.add_separator()
-        self.file_menu.add_command(label="Exit", command=self.destroy)
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        menubar = frame.create_menubar(self)
+        self.configure(menu=menubar)
+        frame.tkraise()                         ## This line will put the frame on front
+ 
 
-        self.edit_menu = tk.Menu(self.menu, tearoff=0)
-        self.menu.add_cascade(label="Edit", menu=self.edit_menu)
-        self.edit_menu.add_command(label="Undo")
-        self.edit_menu.add_command(label="Redo")
-        self.edit_menu.add_separator()
-        self.edit_menu.add_command(label="Cut")
-        self.edit_menu.add_command(label="Copy")
-        self.edit_menu.add_command(label="Paste")
-        self.edit_menu.add_command(label="Delete")
-        self.edit_menu.add_separator()
-        self.edit_menu.add_command(label="Select All")
 
-        self.help_menu = tk.Menu(self.menu, tearoff=0)
-        self.menu.add_cascade(label="Help", menu=self.help_menu)
-        self.help_menu.add_command(label="Help Index")
-        self.help_menu.add_command(label="Contacto", command=oppen_web)
 
-    def create_notebook(self):
-        self.notebook = ttk.Notebook(self)
-        self.notebook.pack(fill="both", expand=True)
+#---------------------------------------- HOME PAGE FRAME / CONTAINER ------------------------------------------------------------------------
 
-        self.create_tab1()
-        self.create_tab2()
-        self.create_tab3()
+class HomePage(tk.Frame):
+    def __init__(self, parent, container):
+        super().__init__(container)
 
-    def create_tab1(self):
-        self.tab1 = ttk.Frame(self.notebook)
-        self.notebook.add(self.tab1, text="Estudio de óbitas")
+        label = tk.Label(self, text="Homme page", font=('Times', '20'))
+        label.pack()
 
-    def create_tab2(self):
-        self.tab2 = ttk.Frame(self.notebook)
-        self.notebook.add(self.tab2, text="Misiones espaciales")
+        ## ADD CODE HERE TO DESIGN THIS PAGE
 
-    def create_tab3(self):
-        self.tab3 = ttk.Frame(self.notebook)
-        self.notebook.add(self.tab3, text="Herraientas")
+    def create_menubar(self, parent):
+        menubar = Menu(parent, bd=3, relief=RAISED, activebackground="#80B9DC")
+
+        ## Filemenu
+        filemenu = Menu(menubar, tearoff=0, relief=RAISED, activebackground="#026AA9")
+        menubar.add_cascade(label="File", menu=filemenu)
+        filemenu.add_command(label="Mission", command=lambda: parent.show_frame(parent.Mission))
+        filemenu.add_command(label="Órbitas", command=lambda: parent.show_frame(parent.PreDetOrb))
+        filemenu.add_command(label="Close", command=lambda: parent.show_frame(parent.HomePage))
+        filemenu.add_separator()
+        filemenu.add_command(label="Exit", command=parent.quit)  
+
+        ## proccessing menu
+        processing_menu = Menu(menubar, tearoff=0)
+        menubar.add_cascade(label="Validation", menu=processing_menu)
+        processing_menu.add_command(label="validate")
+        processing_menu.add_separator()
+
+        ## help menu
+        help_menu = Menu(menubar, tearoff=0, relief=RAISED, activebackground="#026AA9")
+        menubar.add_cascade(label="Help", menu=help_menu)
+        help_menu.add_command(label="About", command = github)
+        help_menu.add_command(label="Contancto", command= linkedin)
+        help_menu.add_separator()
+
+        return menubar
+
+
+#---------------------------------------- PreDetOrb / CONTAINER ------------------------------------------------------------------------
+
+class PreDetOrb(tk.Frame):
+    def __init__(self, parent, container):
+        super().__init__(container)
+
+        label = tk.Label(self, text="Determinnación preliminar de órbitas", font=('Times', '20'))
+        label.pack(pady=0,padx=0)
+
+        ## ADD CODE HERE TO DESIGN THIS PAGE
+
+    def create_menubar(self, parent):
+        menubar = Menu(parent, bd=3, relief=RAISED, activebackground="#80B9DC")
+
+        ## Filemenu
+        filemenu = Menu(menubar, tearoff=0, relief=RAISED, activebackground="#026AA9")
+        menubar.add_cascade(label="File", menu=filemenu)
+        filemenu.add_command(label="Mission", command=lambda: parent.show_frame(parent.Mission))
+        filemenu.add_command(label="Close", command=lambda: parent.show_frame(parent.HomePage))
+        filemenu.add_separator()
+        filemenu.add_command(label="Exit", command=parent.quit)  
+
+        ## proccessing menu
+        processing_menu = Menu(menubar, tearoff=0)
+        menubar.add_cascade(label="Validation", menu=processing_menu)
+        processing_menu.add_command(label="validate")
+        processing_menu.add_separator()
+
+        ## help menu
+        help_menu = Menu(menubar, tearoff=0, relief=RAISED, activebackground="#026AA9")
+        menubar.add_cascade(label="Help", menu=help_menu)
+        help_menu.add_command(label="About", command = github)
+        help_menu.add_command(label="Contancto", command= linkedin)
+        help_menu.add_separator()
+
+        return menubar
+    
+#---------------------------------------- Mission PAGE FRAME / CONTAINER ------------------------------------------------------------------------
+    
+class Mission(tk.Frame):
+    def __init__(self, parent, container):
+        super().__init__(container)
+
+        label = tk.Label(self, text="Configuración de la misión", font=('Times', '20'))
+        label.pack(pady=0,padx=0)
+
+        ## ADD CODE HERE TO DESIGN THIS PAGE
+
+    def create_menubar(self, parent):
+        menubar = Menu(parent, bd=3, relief=RAISED, activebackground="#80B9DC")
+
+        ## Filemenu
+        filemenu = Menu(menubar, tearoff=0, relief=RAISED, activebackground="#026AA9")
+        menubar.add_cascade(label="File", menu=filemenu)
+        filemenu.add_command(label="Órbitas", command=lambda: parent.show_frame(parent.PreDetOrb))
+        filemenu.add_command(label="Close", command=lambda: parent.show_frame(parent.HomePage))
+        filemenu.add_separator()
+        filemenu.add_command(label="Exit", command=parent.quit)  
+
+        ## proccessing menu
+        processing_menu = Menu(menubar, tearoff=0)
+        menubar.add_cascade(label="Validation", menu=processing_menu)
+        processing_menu.add_command(label="validate")
+        processing_menu.add_separator()
+
+        ## help menu
+        help_menu = Menu(menubar, tearoff=0, relief=RAISED, activebackground="#026AA9")
+        menubar.add_cascade(label="Help", menu=help_menu)
+        help_menu.add_command(label="About", command = github)
+        help_menu.add_command(label="Contancto", command= linkedin)
+        help_menu.add_separator()
+
+        return menubar
+
 
 if __name__ == "__main__":
-    gui = GUI()
-    gui.mainloop()
+    app = App()
+    app.mainloop()
+
+
+
+'''
+Enlaces útiles:
+Diseño de tablas:
+    https://pythonguides.com/python-tkinter-table-tutorial/
+
+'''
